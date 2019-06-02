0d4a48dae09ff1a9c9e6afa8fdf2db7afc2bc25a
avsprashant <40791659+avsprashant@users.noreply.github.com>
2019-06-02 20:44:26 +0530

    Update startUpScript.sh

--- startUpScript.sh
+++ startUpScript.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-kubectl create -f helloworld-db-service.yml
-kubectl create -f database-service.yml
 kubectl create -f secrets.yml
+kubectl create -f mysql-pv.yaml
+kubectl create -f database.yml
+kubectl create -f database-service.yml
+kubectl create -f helloworld-db.yml
+kubectl create -f helloworld-db-service.yml
